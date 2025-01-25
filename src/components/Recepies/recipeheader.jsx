1918b0cde4cd46190c02a199c96805d90bd43baf
salmabenhami <benhamisalma2@gmail.com>
2025-01-25 13:36:41 +0100

    star edit

--- src/components/Recepies/recipeheader.jsx
+++ src/components/Recepies/recipeheader.jsx
@@ -19,50 +19,46 @@ const RecipeHeader = () => {
   const users = useSelector((state) => state.auth.users);
   const [isSaved, setIsSaved] = useState(false);
 
-  // Fonction pour afficher les étoiles
   const renderStars = (rating) => {
-    const fullStars = Math.floor(rating); // Nombre d'étoiles pleines
-    const hasHalfStar = rating % 1 !== 0; // Vérifie s'il y a une étoile à moitié pleine
-    const emptyStars = 5 - Math.ceil(rating); // Nombre d'étoiles vides
+    const fullStars = Math.floor(rating); 
+    const hasHalfStar = rating % 1 !== 0;
+    const emptyStars = 5 - Math.ceil(rating); 
 
     const stars = [];
 
-    // Ajouter les étoiles pleines
     for (let i = 0; i < fullStars; i++) {
       stars.push(
         <FontAwesomeIcon
           key={`full-${i}`}
           icon={faStar}
-          style={{ color: '#B55D51', fontSize: '1.2em' }} // Couleur dorée pour les étoiles pleines
+          style={{ color: '#B55D51', fontSize: '1.2em' }}
         />
       );
     }
 
-    // Ajouter l'étoile à moitié pleine si nécessaire
     if (hasHalfStar) {
       stars.push(
         <div key="half-star" style={{ position: 'relative', display: 'inline-block' }}>
           <FontAwesomeIcon
             icon={faStar}
-            style={{ color: '#ccc', fontSize: '1.2em' }} // Couleur grise pour la partie vide
+            style={{ color: '#ccc', fontSize: '1.2em' }} 
           />
           <div style={{ position: 'absolute', top: 0, left: 0, width: '50%', overflow: 'hidden' }}>
             <FontAwesomeIcon
               icon={faStar}
-              style={{ color: '#B55D51', fontSize: '1.2em' }} // Couleur dorée pour la partie pleine
+              style={{ color: '#B55D51', fontSize: '1.2em' }}
             />
           </div>
         </div>
       );
     }
 
-    // Ajouter les étoiles vides
     for (let i = 0; i < emptyStars; i++) {
       stars.push(
         <FontAwesomeIcon
           key={`empty-${i}`}
           icon={faStar}
-          style={{ color: '#ccc', fontSize: '1.2em' }} // Couleur grise pour les étoiles vides
+          style={{ color: '#ccc', fontSize: '1.2em' }} 
         />
       );
     }
@@ -70,17 +66,14 @@ const RecipeHeader = () => {
     return stars;
   };
 
-  // Recherche de la recette en fonction du titre
   const recipe = recipes.find(
     (r) => r.recipeTitle.toLowerCase().replace(/ /g, '-') === title.toLowerCase()
   );
 
-  // Si aucune recette n'est trouvée, retourner un message d'erreur
   if (!recipe) {
     return <p>Recipe not found.</p>;
   }
 
-  // Vérification si les commentaires existent avant d'afficher leur nombre
   const commentCount = recipe.comments ? recipe.comments.length : 0;
 
   return (
